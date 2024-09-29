64fe941ac5ca506abd6d586ecdc4f6a0edeaa3ea
Jaobast <153824183+Jaobast@users.noreply.github.com>
2024-09-29 13:58:49 +0200

    init

---
+++ script/script.js
@@ -0,0 +1,82 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const button = document.querySelector('.jump button');
+
+const jump = () => {
+    mario.classList.add('jump'); 
+
+    setTimeout( () =>{
+        mario.classList.remove('jump'); 
+    }, 700)
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+
+    if(pipePosition <= 85 && pipePosition > 0  && marioPosition < 70){
+        pipe.style.animation ='none';
+        pipe.style.left =`${pipePosition}px`;
+
+
+        mario.style.animation ='none';
+        mario.style.bottom =`${marioPosition}px`;
+
+        mario.src = 'assets/game-over.png ';
+        mario.style.width= '50px'
+        mario.style.marginLeft= '35px'
+
+        clearInterval(loop);
+    }
+}, 10);
+
+document.addEventListener('keydown', jump);
+
+button.addEventListener('click', jump);
+
+
+
+
+
+
+/* const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const button = document.querySelector('.jump button');
+
+const jump = () => {
+    mario.classList.add('jump'); 
+
+    setTimeout( () =>{
+        mario.classList.remove('jump'); 
+    }, 700)
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+
+    if(pipePosition <= 120 && pipePosition > 0  && marioPosition < 105){
+        pipe.style.animation ='none';
+        pipe.style.left =`${pipePosition}px`;
+
+
+        mario.style.animation ='none';
+        mario.style.bottom =`${marioPosition}px`;
+
+        mario.src = 'assets/game-over.png ';
+        mario.style.width= '80px'
+        mario.style.marginLeft= '50px'
+
+        clearInterval(loop);
+    }
+}, 10);
+
+document.addEventListener('keydown', jump);
+
+button.addEventListener('click', jump); */
